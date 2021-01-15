25593a58829386fbb95f1cb2f6e1537e9f717610
sidraaaaa <sidra_rehmani1@hotmail.com>
2021-01-15 11:08:09 -0500

    Second commit

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
+//Breakfast items button working
+//////////////////////////////////////////////////////////////////////
 const addBreakfast = document.querySelector('#add-breakfast');
-const searchContainer = document.querySelector('#search-container')
+const addLunch=document.querySelector("#add-lunch");
+const addDinner=document.querySelector("#add-dinner");
+const searchContainer = document.querySelector('#search-container');
+const form = document.createElement('form');
+const input = document.createElement('input');
 
 addBreakfast.addEventListener('click', () => {
-    console.log('Add breakfast item was clicked');
-    // searchContainer.innerHTML = ''
+    searchContainer.append(form)
+});
+addLunch.addEventListener("click",()=>{
+    searchContainer.append(form)
+});
+addDinner.addEventListener("click",()=>{
     searchContainer.append(form)
 });
 
-const form = document.createElement('form');
-
-const input = document.createElement('input');
 input.setAttribute('type', 'text')
-input.setAttribute('placeholder', 'Search foods')
+input.setAttribute('placeholder', 'Search Food items')
 
 const searchButton = document.createElement('button')
 searchButton.innerText = 'Search'
