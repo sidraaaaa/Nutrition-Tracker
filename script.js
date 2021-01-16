e9bf5da6bd872dd2a19330589486cae9f3784102
sidraaaaa <sidra_rehmani1@hotmail.com>
2021-01-16 13:47:38 -0500

    API settings

--- script.js
+++ script.js
@@ -1,26 +1,123 @@
-//Breakfast items button working
-//////////////////////////////////////////////////////////////////////
-const addBreakfast = document.querySelector('#add-breakfast');
-const addLunch=document.querySelector("#add-lunch");
-const addDinner=document.querySelector("#add-dinner");
-const searchContainer = document.querySelector('#search-container');
-const form = document.createElement('form');
-const input = document.createElement('input');
+const appId = '9e807f51'; 
+const appKey = '4f5feb6b342450f1ac933ab4c7e0d33f'; 
 
-addBreakfast.addEventListener('click', () => {
-    searchContainer.append(form)
+window.addEventListener('DOMContentLoaded', () => {
+    let searchResultsBranded = []
+    let selectedUL = ''
+
+    const calorieCounter = document.querySelector('#calorie-count')
+
+	const addBreakfast = document.querySelector('#add-breakfast');
+	const addLunch = document.querySelector('#add-lunch');
+	const addDinner = document.querySelector('#add-dinner');
+	
+
+    const form = document.createElement('form');
+    
+	const input = document.createElement('input');
+	input.setAttribute('type', 'text');
+    input.setAttribute('placeholder', 'Search foods');
+    
+	const searchButton = document.createElement('button');
+	searchButton.innerText = 'Search';
+	searchButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        searchResultsBranded = []
+        let term = input.value
+        input.value = null
+		fetch(`https://trackapi.nutritionix.com/v2/search/instant?query=${term}`, {
+            headers: {
+                'x-app-id': appId,
+                'x-app-key': appKey,
+            },
+		})
+		.then(res => res.json())
+		.then(res => {
+            console.log(res)
+            searchResultsBranded = res.branded;
+            brandedResultsUl.innerHTML = '';
+            results.append(brandedResultsDiv);
+
+            searchResultsBranded.map(item => {
+                let listItem = document.createElement('div');
+                listItem.className = 'results-li'
+
+                const food = document.createElement('div')
+                food.className = 'food-item'
+                food.innerText = `${item.brand_name_item_name}`;
+
+                const calDiv = document.createElement('div')
+                calDiv.className = 'cal-div'
+
+                const cals = document.createElement('div')
+                cals.innerText = `${item.nf_calories} calories`
+
+                const addButton = document.createElement('button')
+                addButton.className = 'add-delete-button';
+                addButton.innerText = '+'
+                addButton.addEventListener('click', () => {
+                    createLI(item)
+                    brandedResultsUl.innerHTML = '';
+                    searchContainer.innerHTML = '';
+                })
+
+                calDiv.append(cals, addButton)
+                listItem.append(food, calDiv)
+                return brandedResultsUl.append(listItem);
+            });
+        });
+	});
+    form.append(input, searchButton);
+    
+    const results = document.createElement('div')
+    results.id = 'results-div'
+
+    const brandedResultsDiv = document.createElement('div')
+    brandedResultsDiv.className = 'branded-results-div'
+    const brandedResultsUl = document.createElement('div')
+    brandedResultsDiv.append(brandedResultsUl)
+
+	const searchContainer = document.querySelector('#search-container');
+	const buildSearch = () => {
+		searchContainer.innerHTML = '';
+		searchContainer.append(form, results);
+	};
+
+	addBreakfast.addEventListener('click', () => {
+       
+		buildSearch();
+	});
+	addLunch.addEventListener('click', () => {
+        
+		buildSearch();
+    });
+	addDinner.addEventListener('click', () => {
+   
+		buildSearch();
+    });
+    
+    
+    const createLI = food => {
+        calorieCounter.innerText = parseInt(calorieCounter.innerText) + food.nf_calories;
+        const foodLiDiv = document.createElement('div')
+        foodLiDiv.className = 'food-li-div'
+
+        const foodName = document.createElement('div')
+        foodName.innerText = food.food_name
+
+        const deleteButton = document.createElement('button')
+        deleteButton.className = 'add-delete-button';
+        deleteButton.innerText = '-'
+        deleteButton.addEventListener('click', () => {
+            handleDelete(foodLiDiv, food.nf_calories)
+        })
+
+        foodLiDiv.append(deleteButton, foodName)
+        //selectedUL.append(foodLiDiv)
+    }
+
+    const handleDelete = (li, calories) => {
+        li.remove()
+        calorieCounter.innerText = parseInt(calorieCounter.innerText) - calories
+    }
 });
-addLunch.addEventListener("click",()=>{
-    searchContainer.append(form)
-});
-addDinner.addEventListener("click",()=>{
-    searchContainer.append(form)
-});
-
-input.setAttribute('type', 'text')
-input.setAttribute('placeholder', 'Search Food items')
-
-const searchButton = document.createElement('button')
-searchButton.innerText = 'Search'
-
-form.append(input, searchButton)
